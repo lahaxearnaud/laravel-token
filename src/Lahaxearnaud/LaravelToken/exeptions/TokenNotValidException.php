15b04defa309285937604645fbc33366f05e67a2
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2014-12-30 23:00:50 +0100

    add exceptions class

---
+++ src/Lahaxearnaud/LaravelToken/exeptions/TokenNotValidException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
+ * Date: 30/12/14
+ * Time: 22:52
+ */
+
+namespace Lahaxearnaud\LaravelToken\exeptions;
+
+
+class TokenNotValidException extends TokenException {
+
+    public function __construct ($message = "", $code = 0, Exception $previous = NULL)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
